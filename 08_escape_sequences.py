e40469478c14a740e63d3d8c63e1684441490df8
unknown <diloch111@gmail.com>
2024-05-04 00:23:36 +0100

    escape characters

---
+++ 08_escape_sequences.py
@@ -0,0 +1,22 @@
+# Escape characters
+
+# use single quotes to add a quote inside our string
+
+# \"        for escape double quotes
+course = "python's cousre"
+print(course)                       # python's cousre
+
+# \'        for escape single quotes
+course = "python\"s course"
+print(course)                       # python"s course
+
+
+# \\        for escape backslash
+course = "python \\  course"
+print(course)                       # python \  course
+
+
+# \n        for escape n for new line
+course = "python \n course"
+print(course)                       # python
+                                    # course
