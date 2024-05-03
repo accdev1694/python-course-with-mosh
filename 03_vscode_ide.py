ce9b3220c5857175434088a572ea52eeb2c1f007
unknown <diloch111@gmail.com>
2024-05-03 17:55:44 +0100

    linting

---
+++ 03_vscode_ide.py
@@ -0,0 +1,8 @@
+# turn your vscode onto a powerful python ide
+# an ide is an integrated development environment for typing code
+# extensions:
+# python, pylint
+
+# how linting works
+#print "Hello world"     # an invalid code
+# hover over the underlined code to see errors from lint or pylance
