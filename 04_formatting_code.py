e2a67815486cbd013d1a0c6cea035f05c81e23be
unknown <diloch111@gmail.com>
2024-05-03 18:11:09 +0100

    formatting code

---
+++ 04_formatting_code.py
@@ -0,0 +1,14 @@
+# Formatting code
+
+# what are peps?
+# they are python enhancement proposals, basically a guideline on how to write consistent code
+# there is a tool that can help format  your code according to pep8 guidelines
+
+x = 1     # there should be spaces between variables
+# go to comand pallet on your view tab and type in format
+# seletc the formatter and install the auto formatter tool
+# search for autopep8 in extensions and install it
+# go back to view tab and typr formatter and format document
+
+# format on save:
+# go to settings => settings => search format on save and enable it
