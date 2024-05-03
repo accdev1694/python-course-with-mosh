1b547ff04ec2a2d291884ecacddb804742d8cae8
unknown <diloch111@gmail.com>
2024-05-04 00:01:22 +0100

    variables

---
+++ 06_variables.py
@@ -0,0 +1,20 @@
+# used to store values inside a computers memory
+# like a label
+student_count = 1000
+print(student_count)        # 1000
+
+    # Primitive data types:
+# numbers booleans and strings
+
+quantity = 12           # integers
+rating = 2.33           # float
+is_published = True     # booleans meaning yes and no
+course_name = "Python"
+
+    # Some convensions
+# variable names are desciptive and meaningful
+# use lowercase letters to name variables
+# use underscores to seperate words in variable names
+
+
+
