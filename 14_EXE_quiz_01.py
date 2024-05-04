a4ccd8f3e828a7ff67cdb0983ae905ec76cd8452
unknown <diloch111@gmail.com>
2024-05-04 02:06:21 +0100

    01 quiz

---
+++ 14_EXE_quiz_01.py
@@ -0,0 +1,22 @@
+# QUIZ 01:
+# What are the built in primitive types in python
+# strings, numbers and booleans
+
+
+# QUIZ 02:
+fruit = "Apple"
+print(fruit[1])
+# returns p
+
+# QUIZ 03:
+fruit = "Apple"
+print(fruit[1:-1])
+# returns ppl
+
+# QUIZ 04:
+print(10 % 3)
+# returns 1
+
+# QUIZ 05:
+print(bool("False"))
+# returns True
