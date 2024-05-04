5565b8e38b6ed57722ba6021e04c13e824ae0ced
unknown <diloch111@gmail.com>
2024-05-04 01:29:02 +0100

    working with numbers

---
+++ 11_numbers.py
@@ -0,0 +1,51 @@
+# NUMBERS
+
+# integers
+age = 21
+
+# Floats
+price = 4.2
+
+# complex numbers
+x = 1 + 4j
+
+# Arithmetic operations
+# addition
+print(10 + 21)              # 31
+
+# subtraction
+print(21 - 10)              # 11
+
+# multiplication
+print(21 * 10)              # 210
+
+# exponentiation
+print(10 ** 2)              # 100
+
+# division to float
+print(21 / 10)              # 2.1
+
+# division to integer
+print(21 // 10)             # 2
+
+# modulus (remainder)
+print(5 % 4)                # 1
+
+# augmented assigment operator (+= -= *= /=)
+# used to increment something by a value
+x = 5
+print(x)                    # 5
+x = x + 1                   
+print(x)                    # 6
+x += 1                      
+print(x)                    # 7
+x -= 1                      
+print(x)                    # 6
+x *= 2                      
+print(x)                    # 12
+x /= 2                      
+print(x)                    # 6.0
+
+
+
+
