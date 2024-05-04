3596fcc344ea1e80e3744fe4c5f02841e95f48ef
unknown <diloch111@gmail.com>
2024-05-04 03:44:47 +0100

    chaining comparison operators

---
+++ 19_short_circuiting.py
@@ -0,0 +1,16 @@
+# SHORT CIRCUIT EVALUATIONS
+
+# logical operators are short circuit
+
+# for and logical operators
+# python checks the first condition, if it evaluates to the same as
+# the second condition, it keeps checking the next until one of them
+# evaluates to the contrary then it stps and returns false for the
+# entire statement
+
+
+# for or logical operators
+# python checks the value of the first condition, compares it with the next,
+# if they are the same, and if there is any other condition, it checks
+# that one, if it finds it contary, it stops checking and evaluates the entire
+# operation as True
