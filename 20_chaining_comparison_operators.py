2581ea675a291dc51b041173f2fd2841dade772f
unknown <diloch111@gmail.com>
2024-05-04 03:53:56 +0100

    Quiz Time 02

---
+++ 20_chaining_comparison_operators.py
@@ -0,0 +1,8 @@
+# CHAINING COMPARISON OPERATORS
+
+# very important technique for writing clean code
+
+# lets implement a program where age must be between 18 and 65
+age = 22
+message = "Eligible" if 18 <= age <= 65 else "Not Eligible"
+print(message)
---
+++ 21_EXE_Quiz_02.py
@@ -0,0 +1,15 @@
+# QUIZ 01: what will be printed
+
+if 10 == "10":
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+    
+# because
+print(ord("a"))         # 97
+print(ord("b"))         # 98
+print(ord("c"))         # 99
+
+# the answer is c
