32d350e6b692a458ba4d540028ef3adab4b2cfd9
unknown <diloch111@gmail.com>
2024-05-04 01:41:31 +0100

    number methods

---
+++ 12_more_on_numbers.py
@@ -0,0 +1,20 @@
+import math
+# NUMBER FUNCTIONS
+
+qty = 24
+price = 3.6
+temp = -12
+
+# 1. round() - not a dot notation
+print(round(price))             # 4
+
+# 2. abs() - prints the absolute positive number
+print(abs(temp))                # 12
+
+# 3. math module - mathematical functions
+# from the top, import math
+# use dot notation to see all available methods inside math
+print(math.ceil(price))           # 4
+print(math.floor(price))          # 3
+# go to python 3 math module to see all math methods
+
