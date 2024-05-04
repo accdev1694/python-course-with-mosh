f891392e70f0a730440131e71de478a644c742b6
unknown <diloch111@gmail.com>
2024-05-04 02:22:58 +0100

    conditionals

---
+++ 16_conditional_statements.py
@@ -0,0 +1,15 @@
+# CONDITIONALS
+
+# they are used for makinf decisions
+
+temp = 30
+if temp > 20:
+    print("High Temp")
+    print("Keep cool")
+elif temp > 20:
+    print("moderate temp")
+    print("chill out")
+else:
+    print("Its cold")
+# this indicates the end of the if statememnt and will always be executed
+print("Done")
